0eabd417b39f071af5547928ff6b9bedc2c43887
richlloydmiles <richlloydmiles@gmail.com>
2015-05-08 11:47:18 +0200

    01

---
+++ 01/index.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" media="screen" />
+</head>
+<body>
+<h1>Hello World!</h1>	
+</body>
+</html>
